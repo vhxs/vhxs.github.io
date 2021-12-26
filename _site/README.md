dbe1f265e12429c6f5ac8ecf7b2be7f694fb2e3b
vhxs <vikram.saraph.22@gmail.com>
2021-12-26 17:04:37 -0500

    maybe i need to commit _site

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# vhxs.github.io
---
+++ _site/build_and_run_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# clean up _site generated from previous build
+bundle exec jekyll clean
+
+# commands go here
+export BLOG_ROOT=`pwd`
+python scripts/generate_cloud.py
+
+# build site
+bundle exec jekyll build --unpublished
+
+# serve
+bundle exec jekyll serve --unpublished
